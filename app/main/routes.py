4724e877f2504e7eeae897c488319e20adecfbb6
semeykin <semeykin@tochka.com>
2021-12-05 19:43:17 +0500

    step-2 Добавлен подробный просмотр постов

--- app/main/routes.py
+++ app/main/routes.py
@@ -16,8 +16,38 @@ def feed():
     )
 
 
+@bp.route('/posts/<post_id>')
+def post(post_id: int):
+    post = Post.query.filter_by(id=post_id).first_or_404()
 
-@bp.route('/', methods=['GET'])
-@bp.route('/index', methods=['GET'])
-def index():
-    return 'It\'s works'
+    return render_template(
+        'post.html',
+        post=post,
+        title='post',
+    )
+
+
+@bp.route('/search')
+def search():
+    looking_for = request.args.get('s')
+    return render_template('search.html')
+
+
+@bp.route('/user/<username>')
+def user_feed(username: str):
+    return render_template('user-feed.html')
+
+
+@bp.route('/tag/<tag_name>')
+def tag_feed(tag_name: str):
+    return render_template('tag.html')
+
+
+@bp.route('/bookmarks/add/<post_id>', methods=['POST'])
+def add_post_to_bookmarks(post_id: int):
+    return redirect(url_for('/'), code=302)
+
+
+@bp.route('/bookmarks')
+def bookmarks():
+    return render_template('bookmarks.html')
