266711ab611ba20dca24ecaf5c6319676292afb5
semeykin <semeykin@tochka.com>
2021-12-06 09:30:16 +0500

    Летим в хероку

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ load_dotenv(os.path.join(base_dir, '.env'))
 class Config(object):
     SQLALCHEMY_ECHO = os.environ.get('SQLALCHEMY_ECHO') or False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or \
-        'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = (
+            os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+            or 'sqlite:///' + os.path.join(base_dir, 'app.db')
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
